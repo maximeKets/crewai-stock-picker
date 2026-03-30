9d1c0925109a8b74c2fb6a63754ba24f8efd086e
maxime <maximekets80@gmail.com>
2026-03-30 18:30:28 +0200

    #4 feat(tools): add PushNotificationTool for sending user notifications

--- src/crewai_stock_picker/tools/push_tool.py
+++ src/crewai_stock_picker/tools/push_tool.py
@@ -8,3 +8,11 @@ class PushNotification(BaseModel):
     """A message to be sent to the user"""
     message: str = Field(..., description="The message to be sent to the user.")
 
+
+class PushNotificationTool(BaseTool):
+    name: str = "Send a Push Notification"
+    description: str = (
+        "This tool is used to send a push notification to the user."
+    )
+    args_schema: Type[BaseModel] = PushNotification
+
